9920a0299faebd6daeaceb70f081cb52e9210454
remiScript <92605735+remiScript@users.noreply.github.com>
2022-02-28 16:02:52 -0500

    Created in-game clock

---
+++ FF7 Menu/app.js
@@ -0,0 +1,23 @@
+
+let counter = 0;
+let minutes = 0;
+let hours = 0;
+let gameClock = document.getElementById("gameTime");
+
+function formatTime() {
+    counter += 1;
+    if (counter >= 60) {
+        counter = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    gameClock.innerText = hours + ":" + minutes + ":" + counter;
+}
+
+
+
+setInterval(formatTime, 1000);
+

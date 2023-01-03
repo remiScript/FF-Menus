a0c5c36623498ef185b2aec55bbc11479c60c130
remiScript <92605735+remiScript@users.noreply.github.com>
2023-01-03 22:11:11 +0000

    Fixed party swap array methods

--- app.js
+++ app.js
@@ -22,19 +22,68 @@ function formatTime() {
 setInterval(formatTime, 1000);
 
 
-let active = [
-    {name: 'Cloud', level: 7, selected: true}, 
+let activeParty = [
+    {name: 'Cloud', level: 7, selected: false}, 
     {name: 'Tifa', level: 7, selected: false}, 
-    {name: 'Barret', level: 7, selected: false}
+    {name: 'Barret', level: 7, selected: true}
 ]
 
-let reserve = [
+let reserveParty = [
     {name: 'Squall', level: 7, selected: true}, 
     {name: 'Zell', level: 7, selected: false}, 
     {name: 'Irvine', level: 7, selected: false}, 
     {name: 'Edea', level: 7, selected: false}
 ]
 
-// swapping out a party member, cloud for squall
-let activeSwap = active.findIndex(partyMember => active.selected === true);
-active.splice(activeSwap, 1);
+function displayPartyMembers() {
+    console.log("Active Party: ")
+    console.table(activeParty)
+    console.log("Reserve Party: ")
+    console.table(reserveParty)
+    return true;
+}
+
+function swapPartyMembers() {
+    // swapping out a party member, cloud for squall
+    let activeSelected = activeParty.find(partyMember => partyMember.selected === true);
+    let activeIndex = activeParty.findIndex(partyMember => partyMember.selected === true);
+    let reserveSelected = reserveParty.find(partyMember => partyMember.selected === true);
+    let reserveIndex = reserveParty.findIndex(partyMember => partyMember.selected === true);
+    
+    // add cloud to reserve party
+    reserveParty.unshift(activeSelected);
+        
+    // add squall to the active party
+    activeParty.unshift(reserveSelected);
+    console.table(activeParty);
+
+    // remove cloud from active party
+    activeParty.forEach(member => {
+        if (member.name === activeSelected.name){
+            console.log(member.name + ' match')
+            console.log(activeIndex)
+            activeParty.splice(activeIndex + 1, 1)
+            
+        } else {
+            console.log(member.name + ' no match')
+        }
+    });
+    
+    // remove squall from reserve party
+    reserveParty.forEach(member => {
+        if (member.name === reserveSelected.name){
+            console.log(member.name + ' match')
+            console.log(reserveIndex)
+            reserveParty.splice(reserveIndex + 1, 1)
+            
+        } else {
+            console.log(member.name + ' no match')
+        }
+    });
+
+    // activeSelected.selected = false;
+    // reserveSelected.selected = false;    
+
+    console.log('updated!');
+}
+

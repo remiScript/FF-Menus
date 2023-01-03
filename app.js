90dd50476e23b7a5dc78b7427dfecaaf41b7cca0
remiScript <92605735+remiScript@users.noreply.github.com>
2023-01-03 19:28:38 +0000

    Working in array swap methods for party members

--- app.js
+++ app.js
@@ -21,3 +21,20 @@ function formatTime() {
 
 setInterval(formatTime, 1000);
 
+
+let active = [
+    {name: 'Cloud', level: 7, selected: true}, 
+    {name: 'Tifa', level: 7, selected: false}, 
+    {name: 'Barret', level: 7, selected: false}
+]
+
+let reserve = [
+    {name: 'Squall', level: 7, selected: true}, 
+    {name: 'Zell', level: 7, selected: false}, 
+    {name: 'Irvine', level: 7, selected: false}, 
+    {name: 'Edea', level: 7, selected: false}
+]
+
+// swapping out a party member, cloud for squall
+let activeSwap = active.findIndex(partyMember => active.selected === true);
+active.splice(activeSwap, 1);
